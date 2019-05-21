59ca462553da6ac4d2c8d28f282db11e5d9f0ede
Sebastian Weddmark Olsson <visnae@gmail.com>
2019-05-21 19:58:17 +0200

    Add bash format flag

--- src/dos_order_handler.erl
+++ src/dos_order_handler.erl
@@ -11,7 +11,9 @@
         ]).
 
 init(Req, State) ->
-    {cowboy_rest, Req, State}.
+    QueryProps = cowboy_req:parse_qs(Req),
+    Format = proplists:get_value(<<"format">>, QueryProps, <<"json">>),
+    {cowboy_rest, Req, State#{format => Format}}.
 
 allowed_methods(Req, State) ->
     {[<<"POST">>], Req, State}.
@@ -23,9 +25,15 @@ from_json(Req0, State) ->
     {ok, JsonBinary, Req1} = cowboy_req:read_body(Req0),
     case try_order_tasks(JsonBinary) of
         {ok, Ordered} ->
-            {ok, Response} = format_output(Ordered, <<"json">>),
-            Req2 = cowboy_req:set_resp_body(Response, Req1),
-            {true, Req2, State};
+            #{format := Format} = State,
+            case format_output(Ordered, Format) of
+                {ok, Response} ->
+                    Req2 = cowboy_req:set_resp_body(Response, Req1),
+                    {true, Req2, State};
+                {error, _} = Err ->
+                    Req2 = fail_request(Err, Req1),
+                    {false, Req2, State}
+            end;
         {error, _} = Err ->
             Req2 = fail_request(Err, Req1),
             {false, Req2, State}
@@ -47,7 +55,13 @@ try_order_tasks(JsonBinary) ->
 format_output(Tasks, <<"json">>) ->
     ValidKeys = [<<"name">>, <<"command">>],
     Stripped = [maps:with(ValidKeys, T) || T <- Tasks],
-    {ok, jsx:encode(Stripped)}.
+    {ok, jsx:encode(Stripped)};
+format_output(Tasks, <<"bash">>) ->
+    Raw = [[C, $\n] || #{<<"command">> := C} <- Tasks],
+    {ok, iolist_to_binary(["#!/usr/bin/env bash\n\n"|Raw])};
+format_output(_, _) ->
+    {error, unrecognised_format}.
+
 fail_request({error, E} = Err, Req1) ->
     Response = format_error(Err),
     cowboy_req:set_resp_body(Response, Req1).
@@ -57,4 +71,6 @@ format_error({error, not_json_data}) ->
 format_error({error, no_tasks_found}) ->
     <<"Error: The JSON provided does not include any tasks.">>;
 format_error({error, no_topological_order_exist}) ->
-    <<"Error: Impossible to prepare an order of the tasks.">>.
+    <<"Error: Impossible to prepare an order of the tasks.">>;
+format_error({error, unrecognised_format}) ->
+    <<"Error: Format was not recognised.">>.
